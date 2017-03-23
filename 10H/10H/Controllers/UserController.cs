4921c9b922e036e3cbcea6d99c295c76cdcc0f95
Valentin <valentin.cabanel@laposte.net>
2017-03-23 17:54:09 +0100

    creation database2

---
+++ 10H/10H/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Models;
+using ViewModels;
+using System.Web.Mvc;
+using System.Web;
+using System.Web.Http;
+
+namespace _10H.Controllers
+{
+    public class UserController : System.Web.Http.ApiController
+    {
+        private _10HDBContext db = new _10HDBContext();
+
+        //User[] users = new User[]
+        //{
+        //    new User { ID = 1, FirstName = "Tomato Soup", Password = "Groceries"},
+        //    new User { ID = 2, FirstName = "Yo-yo", Password = "Toys" }
+        //};
+
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            List<User> users = db.Users.ToList();
+
+            return users;
+        }
+
+        public IHttpActionResult GetUser(int id)
+        {
+            var user = db.Users.Where(c => c.ID.Equals(id));
+
+           
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+
+     
+}
---
+++ Database10H/Database10H/Music.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Music]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] NVARCHAR(50) NULL, 
+    [artist] NVARCHAR(50) NULL, 
+    [album] NVARCHAR(50) NULL, 
+    [release_date] DATETIME NULL, 
+    [type] NVARCHAR(50) NULL, 
+    [price] FLOAT NULL
+)
